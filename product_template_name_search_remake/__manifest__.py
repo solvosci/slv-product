168d6c94c70af184ef9ec2d1b953272c0d55f003
ChristianSantamaria <christiancameselle@hotmail.com>
2021-10-15 11:52:28 +0200

    [ADD] product_template_name_search_remake

---
+++ product_template_name_search_remake/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Product Template Name Search Remake",
+    "summary": """
+        Adds change to the original name_get method of product.template 
+        due to an error loading a large number of products  
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/solvosci/slv-product",
+    "depends": ["product"],
+    "data": [],
+    'installable': True,
+}
