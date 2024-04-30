31eb399ac0c834752962465f472b91030b76217f
David Alonso // Solvos <david.alonso@solvos.es>
2024-04-30 10:21:03 +0200

    [ADD] product_secondary_unit_factor_digits
    By default, product secondary unit has unlimited precision, but in view are
    only shown two decimals. This makes difficult managing factor with a desired
    higher precision (like e.g. "0.001").
    
    This addon sets a visual [12,6] precision digit for Product Secondary Unit
    factor, that should be enough for the most cases.

---
+++ product_secondary_unit_factor_digits/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Product Secondary Unit - Custom digits for factor field",
+    "summary": """
+        Sets a visual [12,6] precision digit for Product Secondary Unit factor
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "16.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/solvosci/slv-product",
+    "depends": ["product_secondary_unit"],
+    "data": ["views/product_template_views.xml"],
+    "installable": True,
+}
---
+++ product_secondary_unit_factor_digits/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+By default, product secondary unit has unlimited precision, but in view are
+only shown two decimals. This makes difficult managing factor with a desired
+higher precision (like e.g. "0.001").
+
+This addon sets a visual [12,6] precision digit for Product Secondary Unit
+factor, that should be enough for the most cases.
