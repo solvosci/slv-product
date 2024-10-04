d7d2fe2e9c3d80fdf1b79e3bd1939ec64b8249f2
Ángel Rivas <angel.rivas@solvos.es>
2024-10-04 11:29:57 +0200

    [ADD] product_price_discounted_visibility

---
+++ product_price_discounted_visibility/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+{
+    "name": "Product Price discounted visibility",
+    "summary": """
+        Make visible price_discounted in in supplier pricelist.
+        """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/solvosci/slv-product",
+    "depends": ["product"],
+    "data": [
+        "views/product_supplierinfo_views.xml",
+    ],
+    "installable": True,
+}
