4821074aa56dc4de4658c74802fa0d78ff1befac
Ángel Rivas <angel.rivas@solvos.es>
2024-10-07 10:16:01 +0200

    [ADD] product_category_pricelist_button

---
+++ product_category_pricelist_button/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+{
+    "name": "Product category pricelist button",
+    "summary": """
+        Adds a smart button to product categories linking to associated pricelists items.
+        """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/solvosci/slv-product",
+    "depends": ["product"],
+    "data": [
+        "views/product_category_views.xml",
+    ],
+    "installable": True,
+}
