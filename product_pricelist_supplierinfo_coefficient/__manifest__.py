11209fe7d10c0175a4e0db6e9b429b45745bdddc
Ángel Rivas <angel.rivas@solvos.es>
2024-10-14 09:13:06 +0200

    [ADD] product_pricelist_supplierinfo_coefficient(2)

--- product_pricelist_supplierinfo_coefficient/__manifest__.py
+++ product_pricelist_supplierinfo_coefficient/__manifest__.py
@@ -1,5 +1,5 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Product Pricelist Supplierinfo Coefficient",
--- product_pricelist_supplierinfo_coefficient/models/product_pricelist_item.py
+++ product_pricelist_supplierinfo_coefficient/models/product_pricelist_item.py
@@ -1,5 +1,5 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
@@ -12,7 +12,7 @@ class ProductPricelistItem(models.Model):
     def _compute_price(self, product, quantity, uom, date, currency):
         result = super()._compute_price(product, quantity, uom, date, currency=currency)
 
-        # Precio sin descuento
+        # Price without discount
         price_without_discount = product.sudo()._get_supplierinfo_pricelist_price(
             self,
             date=date or self.env.context.get("date"),
@@ -22,7 +22,7 @@ class ProductPricelistItem(models.Model):
         discount = 0.0
         seller = None 
 
-        # Precio según la fórmula
+        # Price according to the formula
         if self.compute_price == "formula" and self.base == "supplierinfo":
             seller = product.sudo()._select_seller(
                 partner_id=self.env.context.get("force_filter_supplier_id"),
@@ -31,32 +31,32 @@ class ProductPricelistItem(models.Model):
             )
 
             if seller:
-                # Obtener el precio
+                # Get price
                 price_discounted = seller._get_supplierinfo_pricelist_price()
                 discount = seller.discount or 0.0
 
-                # Aplico descuento si existe
+                # Apply discount if it exists
                 if discount > 0:
                     price_discounted *= (1 - (discount / 100))
                 else:
-                    price_discounted = price_without_discount  # Si no hay descuento, usar el precio sin descuento
+                    price_discounted = price_without_discount  # If not discount, used to price without discount
 
-                # Si el coeficiente es mayor a 0, aplica el coeficiente al precio descontado
+                # If coefficient is > to 0, applu coefficient to price without discount
                 if self.coefficient > 0:
                     result = price_discounted * self.coefficient
                 else:
-                    # Si el coeficiente es 0, verificamos si hay descuento
+                    # If coeffient = 0, verify if there is discount
                     if discount == 0:
-                        # Si el descuento también es 0, devuelve 999999
+                        # If discount = 0, return 999999
                         result = 999999
                     else:
-                        # Si hay descuento, devuelve el precio sin descuento
+                        # If there is discount, return price without discount
                         result = price_without_discount
             else:
-                # Si no hay vendedor, simplemente devuelve el precio sin descuento
+                # If there isnt  seller, return price without discount
                 result = price_without_discount
         else:
-            # Si compute_price no es "formula", devuelve el precio sin descuento por defecto
+            # If compute_price isnt "formula", return price without discount default
             result = price_without_discount
 
         return result
