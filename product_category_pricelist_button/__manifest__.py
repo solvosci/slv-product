becd32edb22b9f51013ef9e11d446c480fb3e165
Ángel Rivas <angel.rivas@solvos.es>
2024-10-28 15:56:59 +0100

    [IMP] product_category_pricelist_button: moved legend from Pricelist Items to Price Rules

--- product_category_pricelist_button/__manifest__.py
+++ product_category_pricelist_button/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Product category pricelist button",
     "summary": """
-        Adds a smart button to product categories linking to associated pricelists items.
+        Adds a smart button to product categories linking to associated price rules.
         """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "category": "Product",
     "website": "https://github.com/solvosci/slv-product",
     "depends": ["product"],
