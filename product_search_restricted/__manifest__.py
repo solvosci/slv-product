032053a33d9a405d69d9c746c62897d26cd73de0
David Alonso // Solvos <david.alonso@solvos.es>
2026-01-02 14:47:28 +0100

    [ADD] product_search_restricted
    For large databases, looking for products across inline `Many2one` searches e.g. within a sales order line could collapse database.
    
    With this addon installed it becomes less possible, because at lear five (5) characteres are required.

---
+++ product_search_restricted/__manifest__.py
@@ -0,0 +1,11 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Product - limit search to a minumum number of characters",
+    "version": "18.0.1.0.0",
+    "depends": ["product"],
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-product",
+    "license": "AGPL-3",
+}
---
+++ product_search_restricted/models/product_template.py
@@ -0,0 +1,15 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import api, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        # Restrict searches to at least 5 characters
+        if len(name) < 5:
+            return []
+        return super().name_search(name=name, args=args, operator=operator, limit=limit)
